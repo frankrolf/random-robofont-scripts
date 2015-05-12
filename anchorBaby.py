1f77e37ab5de56b7c19e7d7f51b9630865de9535
Frank Grießhammer <frankrolf@gmail.com>
2015-05-11 22:02:46 -0700

    Add documentation to the script.

--- anchorBaby.py
+++ anchorBaby.py
@@ -1,19 +1,22 @@
 '''
-anchorBaby.py
--------------
+## anchorBaby.py
+
+RoboFont script for keeping anchors consistent.
 
 Inspired by an idea from James T. Edmondson, this script will take note of an 
 anchor movement in a given glyph, and replicate that movement in all dependent
-glyphs.
-For instance, if a font has the glyphs /a, /a.init and /a.fina; and all three 
-have an anchors named "top"; any dragging of this anchor will be replicated 
-in all the other glyphs. This way, anchor positions stay consistent.
+glyphs.  
 
+For instance, if a font has the glyphs `a` `a.init` and `a.fina`; and all three 
+have an anchors named “top”; any dragging of that anchor will be replicated 
+in all the dependent glyphs.  
 It does not matter which glyph the anchor is dragged in. Anchor names also
 do not matter, as long as they are consistent among dependent glyphs.
 
-This was a good trip into the world of observers.
-Thanks for the opportunity, James! :)
+Ideally, this script is run as a RoboFont startup script.
+
+Writing this was a good trip into the world of observers.
+Thanks for the opportunity, James! :-)
 '''
 
 from mojo.events import addObserver, removeObserver
